79ecbece2a627ee03e8169c754c59cc214c8848d
socallaghan1-bit <socallaghan1@woolworths.com.au>
2026-09-24 10:47:24 +0930

    Add service worker for PWA caching

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'tdu-pwa-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './events.json',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cached) => cached || fetch(e.request))
+    );
+});
