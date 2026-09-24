030aee79aba719a8ce5a951b667f9e752da8eadf
socallaghan1-bit <socallaghan1@woolworths.com.au>
2026-09-24 12:17:44 +0930

    Replace placeholder schedule with official 2027 TDU stages

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tdu-pwa-v2';
+const CACHE_NAME = 'tdu-pwa-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -10,9 +10,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
     self.skipWaiting();
 });
 
